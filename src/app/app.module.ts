6748e394eb9355cb8ffe24260a9d1de418e77223
Sid <sidatycheikh93@gmail.com>
2022-10-10 20:25:43 +0200

    Ajouter des composant : nav-bar, property-card, property-list + qlq extentions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PropertyCardComponent } from './property/property-card/property-card/property-card.component';
+import { PropertListComponent } from './property/propert-list/propert-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    PropertyCardComponent,
+    PropertListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
