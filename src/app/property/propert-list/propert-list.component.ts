af3462cd70d86716d1be4cf09b815ca7cd56bc80
Sid <sidatycheikh93@gmail.com>
2022-10-11 09:44:51 +0200

    Passing data between components, using ngfor directive, using proprety binding

--- src/app/property/propert-list/propert-list.component.ts
+++ src/app/property/propert-list/propert-list.component.ts
@@ -6,7 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./propert-list.component.css']
 })
 export class PropertListComponent implements OnInit {
-
+  properties: Array<any> = [
+    {
+    "Id": 1,
+    "Name":"Birla House",
+    "Type":"House",
+    "Price":12000
+    },
+    {
+      "Id": 2,
+      "Name":"Erose Villa",
+      "Type":"House",
+      "Price":15000
+      },
+      {
+        "Id": 3,
+        "Name":"Pearl White House",
+        "Type":"House",
+        "Price":18000
+        },
+        {
+        "Id": 3,
+        "Name":"Pearl White House",
+        "Type":"House",
+        "Price":18000
+        },
+        {
+          "Id": 3,
+          "Name":"Pearl White House",
+          "Type":"House",
+          "Price":18000
+          },
+          {
+            "Id": 3,
+            "Name":"Pearl White House",
+            "Type":"House",
+            "Price":18000
+            }
+]
   constructor() { }
 
   ngOnInit() {
--- src/app/property/property-card/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -6,12 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-card.component.css']
 })
 export class PropertyCardComponent implements OnInit {
+@Input() property : any
 
-  Proprety:any = {
-    "Id": 1,
-    "Type":"House",
-    "Price":12000
-  }
   constructor() { }
 
   ngOnInit() {
