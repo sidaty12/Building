9a101b69dc4b5f33e3f546f2857fae7a740af59b
Sid <sidatycheikh93@gmail.com>
2022-11-17 11:43:37 +0100

    #40 Handling Errors Globally in ASP .NET Core Web API

---
+++ API/Extentions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using System.Net;
+
+namespace API.Extentions
+{
+  public static class ExceptionMiddlewareExtensions
+  {
+    
+    public static void ConfigureExeptionHandeler(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      else
+      {
+        app.UseExceptionHandler(
+           options =>
+           {
+             options.Run(
+               async context =>
+
+      {
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        var ex = context.Features.Get<IExceptionHandlerFeature>();
+        if (ex != null)
+        {
+          await context.Response.WriteAsync(ex.Error.Message);
+        }
+      });
+           });
+      }
+
+    }
+  }
+}
