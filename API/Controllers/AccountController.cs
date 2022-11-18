ee3407473bb04082d0f8c33d3412532151d0aff7
Sid <sidatycheikh93@gmail.com>
2022-11-18 07:41:44 +0100

    #44 Create JWT in ASP.NET Core and use it for Authorization, how to creat a token

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,7 +1,13 @@
 using API.Dtos;
 using API.Interfaces;
+using API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -26,16 +32,44 @@ namespace API.Controllers
 
       var user = await uow.UserRepository.Authenticate(loginReq.UserName, loginReq.Password);
 
-      if(user == null)
+      if (user == null)
       {
         return Unauthorized();
       }
 
       var loginRes = new LoginResDto();
       loginRes.UserName = user.Username;
-      loginRes.Token = "Token to be generated";
+      loginRes.Token = CreateJWT(user);
+                                                   
 
       return Ok(loginRes);
     }
+
+    private string CreateJWT(User user)
+    {
+      var key = new SymmetricSecurityKey(Encoding.UTF8
+        .GetBytes("shhh.. this is my top secret"));
+
+      var claims = new Claim[] {
+        new Claim(ClaimTypes.Name, user.Username),
+        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+      };
+
+      var signingCredentials = new SigningCredentials(
+        key, SecurityAlgorithms.HmacSha256Signature);
+
+      var tokenDescriptor = new SecurityTokenDescriptor
+      {
+        Subject = new ClaimsIdentity(claims),
+        Expires = DateTime.UtcNow.AddMinutes(1),
+        SigningCredentials = signingCredentials
+      };
+
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return tokenHandler.WriteToken(token);
+
+    }
   }
+
 }
