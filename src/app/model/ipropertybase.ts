b0bebb7371b0774492a19b9e2eb41eb1c0897382
Sid <sidatycheikh93@gmail.com>
2023-01-22 00:15:03 +0100

    #57 Populate list page data from API

--- src/app/model/ipropertybase.ts
+++ src/app/model/ipropertybase.ts
@@ -1,15 +1,15 @@
 export interface  IPropertyBase {
 
-  Id: number;
-  SellRent: number;
-  Name: string;
-  PType: string;
-  FType: string;
-  Price: number;
-  BHK: number;
-  BuiltArea: number;
-  City : string;
-  RTM: number;
-  Image?: string;
+  id: number;
+  sellRent: number;
+  name: string;
+  propertyType: string;
+  furnishingType: string;
+  price: number;
+  bhk: number;
+  builtArea: number;
+  city : string;
+  readyToMove: number;
+  image?: string;
 
 }
