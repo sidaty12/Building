5c97fa1cab43c798798919fa5c166ed1dd6a8e14
Sid <sidatycheikh93@gmail.com>
2023-02-10 18:10:34 +0100

    #58 Hookup Property Detail View to Web Api, Update Controller, Add DTO

--- API/Controllers/PropertyDetailDto.cs
+++ API/Controllers/PropertyDetailDto.cs
@@ -1,6 +1,21 @@
-﻿namespace API.Controllers
+using API.Dtos;
+using System;
+
+namespace API.Controllers
 {
-  internal class PropertyDetailDto
+  public class PropertyDetailDto : PropertyListDto
   {
+    public int CarpetArea { get; set; }
+    public string Address { get; set; }
+    public string Address2 { get; set; }
+    public int FloorNo { get; set; }
+    public int TotalFloors { get; set; }
+    public string MainEntrance { get; set; }
+    public int Security { get; set; }
+    public bool Gated { get; set; }
+    public int Maintenance { get; set; }
+    public DateTime EstPossessionOn { get; set; }
+    public int Age { get; set; }
+    public string Description { get; set; }
   }
 }
