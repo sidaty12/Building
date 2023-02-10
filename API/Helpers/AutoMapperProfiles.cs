fca0a8ea1cd77a669519c437daeec56226cf225e
Sid <sidatycheikh93@gmail.com>
2023-02-10 18:16:58 +0100

    #58 Creat Map

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -9,8 +9,16 @@ namespace API.Helpers
     public AutoMapperProfiles()
     {
       CreateMap<City, CityDto>().ReverseMap();
+
       CreateMap<City, CityUpdateDto>().ReverseMap();
+
       CreateMap<Property, PropertyListDto>()
+          .ForMember(d => d.City, opt => opt.MapFrom(src => src.City.Name))
+          .ForMember(d => d.Country, opt => opt.MapFrom(src => src.City.Country))
+          .ForMember(d => d.PropertyType, opt => opt.MapFrom(src => src.PropertyType.Name))
+          .ForMember(d => d.FurnishingType, opt => opt.MapFrom(src => src.FurnishingType.Name));
+
+      CreateMap<Property, PropertyDetailDto>()
       .ForMember(d => d.City, opt => opt.MapFrom(src => src.City.Name))
       .ForMember(d => d.Country, opt => opt.MapFrom(src => src.City.Country))
       .ForMember(d => d.PropertyType, opt => opt.MapFrom(src => src.PropertyType.Name))
