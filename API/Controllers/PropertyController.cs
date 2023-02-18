68cefa92301a18aa9f54397dea46151714a57497
Sid <sidatycheikh93@gmail.com>
2023-02-18 18:14:51 +0100

    #67 Setting Primary Photo

--- API/Controllers/PropertyController.cs
+++ API/Controllers/PropertyController.cs
@@ -13,39 +13,39 @@ using System;
 namespace API.Controllers
 {
   public class PropertyController : BaseController
+  {
+    private readonly IUnitOfWork uow;
+    private readonly IMapper mapper;
+    private readonly IPhotoService photoService;
+
+    public PropertyController(IUnitOfWork uow,
+    IMapper mapper,
+    IPhotoService photoService)
     {
-        private readonly IUnitOfWork uow;
-        private readonly IMapper mapper;
-        private readonly IPhotoService photoService;
-
-        public PropertyController(IUnitOfWork uow,
-        IMapper mapper,
-        IPhotoService photoService)
-        {
-          //  this.photoService = photoService;
-            this.uow = uow;
-            this.mapper = mapper;
+      //  this.photoService = photoService;
+      this.uow = uow;
+      this.mapper = mapper;
       this.photoService = photoService;
-        }
+    }
 
-          //property/list/1
-        [HttpGet("list/{sellRent}")]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetPropertyList(int sellRent)
-        {
-            var properties = await uow.PropertyRepository.GetPropertiesAsync(sellRent);
-            var propertyListDTO = mapper.Map<IEnumerable<PropertyListDto>>(properties);
-            return Ok(propertyListDTO);
-        }
+    //property/list/1
+    [HttpGet("list/{sellRent}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetPropertyList(int sellRent)
+    {
+      var properties = await uow.PropertyRepository.GetPropertiesAsync(sellRent);
+      var propertyListDTO = mapper.Map<IEnumerable<PropertyListDto>>(properties);
+      return Ok(propertyListDTO);
+    }
 
-        [HttpGet("detail/{id}")]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetPropertyDetail(int id)
-        {
-            var property = await uow.PropertyRepository.GetPropertyDetailAsync(id);
-            var propertyDTO = mapper.Map<PropertyDetailDto>(property);
-            return Ok(propertyDTO);
-        }
+    [HttpGet("detail/{id}")]
+    [AllowAnonymous]
+    public async Task<IActionResult> GetPropertyDetail(int id)
+    {
+      var property = await uow.PropertyRepository.GetPropertyDetailAsync(id);
+      var propertyDTO = mapper.Map<PropertyDetailDto>(property);
+      return Ok(propertyDTO);
+    }
 
     [HttpPost("add")]
     [Authorize]
@@ -66,7 +66,7 @@ namespace API.Controllers
     public async Task<IActionResult> AddPropertyPhoto(IFormFile file, int propId)
     {
       var result = await photoService.UploadPhotoAsync(file);
-      if(result.Error != null)
+      if (result.Error != null)
         return BadRequest(result.Error.Message);
 
       var property = await uow.PropertyRepository.GetPropertyByIdAsync(propId);
@@ -86,6 +86,40 @@ namespace API.Controllers
       await uow.SaveAsync();
       return StatusCode(201);
     }
-   
+
+    [HttpPost("set-primary-photo/{propId}/{photoPublicId}")]
+    [Authorize]
+    public async Task<IActionResult> SetPrimaryPhoto(int propId, string photoPublicId)
+    {
+      var userId = GetUserId();
+
+      var property = await uow.PropertyRepository.GetPropertyByIdAsync(propId);
+
+      if (property == null)
+      {
+        return BadRequest("No such property or photo exists");
+      }
+      var P = property.PostedBy;
+      if (P != userId)
+      {
+        return BadRequest("You are not authorised to change the photo");
+      }
+
+      var photo = property.Photos.FirstOrDefault(p => p.PublicId == photoPublicId);
+
+      if (photo == null)
+        return BadRequest("No such property or photo exists");
+
+      if (photo.IsPrimary)
+        return BadRequest("This is already a primary photo");
+
+      var currentPrimary = property.Photos.FirstOrDefault(p => p.IsPrimary);
+      if (currentPrimary == null) currentPrimary.IsPrimary = false;
+      photo.IsPrimary = true;
+
+      if (await uow.SaveAsync()) return NoContent();
+
+      return BadRequest("Some error has occured, failed to set primary photo");
+    }
   }
 }
