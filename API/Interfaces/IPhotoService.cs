617c4f584e23d7d1ba4ae851f65755a9338b0b16
Sid <sidatycheikh93@gmail.com>
2023-02-18 20:53:22 +0100

    #68 Deleting a photo -API

--- API/Interfaces/IPhotoService.cs
+++ API/Interfaces/IPhotoService.cs
@@ -7,5 +7,8 @@ namespace API.Interfaces
   public interface IPhotoService
   {
     Task<ImageUploadResult> UploadPhotoAsync(IFormFile photo);
+
+    Task<DeletionResult> DeletePhotoAsync(string publicId);
+
   }
 }
